45876a9099c81b7929185a59dce4dcfea4b324cf
JimcostDev <rjak727@gmail.com>
2022-07-14 15:25:48 +0200

    crear tabla - edit

--- scripts_introduccion/crear_tabla.sql
+++ scripts_introduccion/crear_tabla.sql
@@ -1,4 +1,4 @@
-CREATE TABLE [dbo].[Paciente_1](
+CREATE TABLE [dbo].[Paciente](
 	[IdPaciente] [int] IDENTITY(1,1) NOT NULL,
 	[Nombre] [varchar](50) NULL,
 	[Apellido] [varchar](80) NULL,
@@ -8,5 +8,15 @@ CREATE TABLE [dbo].[Paciente_1](
 	[IdPais] [char](3) NULL,
 	[Email] [varchar](50) NULL,
 	[Observacion] [varchar](500) NULL,
-	CONSTRAINT [PK_Paciente_1] PRIMARY KEY (IdPaciente)
+	CONSTRAINT [PK_Paciente] PRIMARY KEY (IdPaciente)
 );
+
+CREATE TABLE Pago (
+IdPago INT IDENTITY (1,1) NOT NULL,
+Concepto TINYINT NOT NULL,
+Fecha DATETIME NOT NULL,
+Monto MONEY NOT NULL,
+Estado TINYINT NOT NULL,
+Observacion VARCHAR(1000),
+CONSTRAINT PK_Pago PRIMARY KEY (IdPago)
+)
