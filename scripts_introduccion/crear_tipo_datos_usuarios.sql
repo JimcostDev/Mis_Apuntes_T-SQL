2fe9d4b7cca6b2d42c31d8d683b327db4fe0b3a0
JimcostDev <rjak727@gmail.com>
2022-07-14 16:15:41 +0200

    crear tipos de datos de usuario que faltaron

--- scripts_introduccion/crear_tipo_datos_usuarios.sql
+++ scripts_introduccion/crear_tipo_datos_usuarios.sql
@@ -6,8 +6,13 @@ CREATE TYPE [dbo].[observacion] FROM [varchar](1000) NULL
 GO
 CREATE TYPE [dbo].[paciente] FROM [int] NOT NULL
 GO
-CREATE TYPE [dbo].[pais] FROM [char](3) NULL
+CREATE TYPE [dbo].[pais] FROM [char](3) NOT NULL
 GO
 CREATE TYPE [dbo].[turno] FROM [int] NOT NULL
 GO
-
+CREATE TYPE [dbo].[especialidad] FROM [int] NOT NULL
+GO
+CREATE TYPE [dbo].[concepto] FROM [int] NOT NULL
+GO
+CREATE TYPE [dbo].[pago] FROM [int] NOT NULL
+GO
